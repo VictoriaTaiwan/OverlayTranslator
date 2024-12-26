ed41cdc129adf1f352e00ad2114ab2688ffb6c43
Iri <beatokaine@gmail.com>
2024-12-26 15:33:08 +0800

    - Moved logic to Main class

--- src/main.py
+++ src/main.py
@@ -4,10 +4,29 @@ import os
 sys.dont_write_bytecode = True
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
+from PyQt5.QtWidgets import QApplication
+from PIL import ImageGrab
+from data.ocr.tesseract import Ocr
 from data.translation.translator import Translator
 from data.translation.translator_service import SERVICE
+from ui.overlay import Overlay
 
+class Main:
+    def __init__(self):
+        self.translator = Translator()
+        self.ocr = Ocr()
+
+    def onScreenshotTranslate(self, bbox):
+        captured_image = ImageGrab.grab(bbox=bbox) 
+        ocrResult = self.ocr.imageToText(captured_image)
+        print(ocrResult)
+            
+        translation = self.translator.translate(SERVICE.DEEPL, ocrResult)
+        print(translation)  
+        
 if __name__ == "__main__":
-    translator = Translator()
-    response = translator.translate(SERVICE.GOOGLE, r"Привет. Как твои дела? Все ли хорошо?")
-    print(f"Response Text: {response}")
+    main = Main()
+    app = QApplication(sys.argv)
+    window = Overlay(onMouseReleased=main.onScreenshotTranslate)
+    window.showMaximized()
+    sys.exit(app.exec())     
--- src/ui/overlay.py
+++ src/ui/overlay.py
@@ -1,44 +1,29 @@
-import sys
-from PyQt5.QtCore import Qt, QRect, QPoint, QTimer
+from PyQt5.QtCore import Qt, QRect, QPoint
 from PyQt5 import QtGui
-from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtWidgets import QMainWindow
 from pynput.keyboard import Key, Listener
-from translated_widget import TranslatedWidget
-from PyQt5.QtWidgets import *
-from PIL import ImageGrab
-from data.ocr.tesseract import Ocr
-from data.translation.translator import Translator
-from data.translation.translator_service import SERVICE
 
 class Overlay(QMainWindow):
-    def __init__(self):
+    def __init__(self, onMouseReleased):
         super().__init__()
 
         self.setWindowTitle("Overlay Translator")
         self.setMouseTracking(True)
+        self.onMouseReleased = onMouseReleased
 
         # Window flags for frameless, transparent, and always-on-top window
         self.setWindowFlags(Qt.WindowStaysOnTopHint| Qt.FramelessWindowHint)
         self.setAttribute(Qt.WA_TranslucentBackground)
+        
+        # Start the keyboard listener
+        listener = Listener(on_press=self.on_press)
+        listener.start()
         
         self.can_draw = False
         self.isMouseClicked = False
         
         self.begin = QPoint()
         self.end = QPoint()
-
-        # Create and add multiple TranslatedWidgets
-        self.translated_widgets = []        
-    
-    def add_widget_at_position(self, text):
-        # Create a new TranslatedWidget and set its position based on mouse click
-        widget = TranslatedWidget()
-        widget.setLabelText(text)  # Set the label text
-        width = abs(self.begin.x() - self.end.x())
-        height = abs(self.begin.y() - self.end.y())
-        widget.setGeometry(self.begin.x(), self.begin.y(), width, height)  # Set geometry based on mouse click position
-        self.translated_widgets.append(widget)  # Add widget to the list
-        widget.show()
 
     def showOverlay(self):
         self.can_draw = not self.can_draw
@@ -66,80 +51,31 @@ class Overlay(QMainWindow):
     def mousePressEvent(self, event):
         # Handle mouse press event to add a widget
         if event.button() == Qt.LeftButton:
-            window.isMouseClicked = True 
+            self.isMouseClicked = True 
             click_position = event.pos()
-            print(f"Mouse clicked at: {click_position}")  # Debugging line
+            print(f"Mouse clicked at: {click_position}")
             self.begin = event.pos()
             self.end = event.pos()
             self.update()
     
     def mouseReleaseEvent(self, event):
         if event.button() == Qt.LeftButton:
-            print('Released')
-            window.isMouseClicked = False
-            #self.add_widget_at_position("NEW WIDGET")
+            print('Mouse released')
+            self.isMouseClicked = False
             self.update()
             
-            #width = abs(self.begin.x() - self.end.x())
-            #height = abs(self.begin.y() - self.end.y())
             bbox = (self.begin.x(), self.begin.y(), self.end.x(), self.end.y()) 
             print(bbox)
-            
-            captured_image = ImageGrab.grab(bbox=bbox) 
-            ocr = Ocr()
-            ocr.imageToText(captured_image)
-            print(ocr)
-            
-            translator = Translator()
-            translation = translator.translate(SERVICE.DEEPL, ocr)
-            print(translation)
+            self.onMouseReleased(bbox)
             
     
     def mouseMoveEvent(self, event):
-        if(window.isMouseClicked):
+        if(self.isMouseClicked):
             print('Mouse dragged')
-            window.end = event.pos()
-            window.update()                                   
+            self.end = event.pos()
+            self.update()                                   
 
-def on_press(key):
-    if key == Key.up:  # Toggle overlay on "Up" key press
-        window.showOverlay()
-        window.update()
-'''        
-def on_mouse_click(x, y, button, pressed):
-    point = QPoint(x, y)
-    if button == mouse.Button.left and window.can_draw:   
-        if(pressed):
-            print(f"Mouse clicked at: {x} {y}")
-            window.begin = point
-            window.end = point
-        else:
-            print(f'Mouse released at: {x} {y}')           
-            #QTimer.singleShot(0, lambda: window.add_widget_at_position(point))
-    window.isMouseClicked = pressed       
-    window.update()       
-
-def on_mouse_drag(x, y):
-    if(window.isMouseClicked and window.can_draw):
-        print('Mouse dragged')
-        window.end = QPoint(x, y)
-        window.update()     
-'''
-
-if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    window = Overlay()
-    window.showMaximized()  # Make the window fullscreen for the overlay
-
-    # Start the keyboard listener
-    listener = Listener(on_press=on_press)
-    listener.start()
-    
-    '''
-    mouse_listener = mouse.Listener(
-    on_move=on_mouse_drag,
-    on_click=on_mouse_click)
-    mouse_listener.start()
-    '''
-    # Run the PyQt application
-    sys.exit(app.exec())
+    def on_press(self, key):
+        if key == Key.up:  # Toggle overlay on "Up" key press
+            self.showOverlay()
+            self.update()
